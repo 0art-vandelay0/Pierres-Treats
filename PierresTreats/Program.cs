263b9b51aecbc6390260bb17ea10744ecce8b932
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-08-11 10:46:02 -0700

    Add Program file with appropriate configurations

---
+++ PierresTreats/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PierresTreats.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace PierresTreats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<PierresTreatsContext>(
+                            dbContextOptions => dbContextOptions
+                                .UseMySql(
+                                    builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                                    )
+                                )
+                            );
+
+            // new lines with Identity
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<PierresTreatsContext>().AddDefaultTokenProviders();
+
+            WebApplication app = builder.Build();
+
+            // app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
