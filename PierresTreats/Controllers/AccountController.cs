744c6d45e307aa76b5b751cb3a49bdd1e1837f54
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-08-11 11:02:56 -0700

    Add Account Controller with index and register

---
+++ PierresTreats/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using PierresTreats.Models;
+using PierresTreats.ViewModels;
+
+namespace PierresTreats.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly PierresTreatsContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierresTreatsContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
