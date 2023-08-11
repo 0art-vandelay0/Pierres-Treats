cc31fa28c2318d45f8d9f8b752cb497998730266
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-08-11 11:23:01 -0700

    Add Flavor controller with index, create, details

---
+++ PierresTreats/Controllers/FlavorController.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierresTreats.Controllers
+{
+    public class FlavorsController : Controller
+    {
+        private readonly PierresTreatsContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public FlavorsController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+            List<Flavor> userFlavors = _db.Flavors.Where(entry => entry.User.Id == currentUser.Id).Include(flavor => flavor.JoinEntities).ThenInclude(join => join.Treat).ToList();
+            return View(userFlavors);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Flavor flavor, int TreatId)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+                return View(flavor);
+            }
+            else
+            {
+                string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+                flavor.User = currentUser;
+                _db.Flavors.Add(flavor);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            Flavor thisFlavor = _db.Flavors
+                .Include(flavor => flavor.JoinEntities)
+                .ThenInclude(join => join.Treat)
+                .FirstOrDefault(flavor => flavor.FlavorId == id);
+            return View(thisFlavor);
+        }
+
+    }
+
+}
